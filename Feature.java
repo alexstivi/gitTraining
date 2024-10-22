099593ee0a19052f243458f8d183d9423a033dfd
alex_stivi <alexstividor@gmail.com>
2024-10-22 16:26:07 +0200

    clean feature class

---
+++ Feature.java
@@ -0,0 +1,12 @@
+package ru.alex.lesson7;
+
+public class Feature {
+    private String name;
+    private Integer count;
+
+    public Feature() {};
+
+    public String getName() {
+        return name;
+    }
+}
--- feature.java
+++
@@ -1,9 +0,0 @@
-public class Feature {
-	private String name;
-	private Integer count;
-
-	public Feature() {};
-
-	public String getName() {
-		return name;
-}
